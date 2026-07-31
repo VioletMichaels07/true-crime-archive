3f73ff960713c1d4650fad8d995dc3a8c42fe8ba
VioletMichaels07 <Violet.Donna.Michaels07@protonmail.com>
2026-07-31 09:29:22 -0400

    Add script.js to log current year and archive load

---
+++ script.js
@@ -0,0 +1,9 @@
+const year = new Date().getFullYear();
+
+console.log(`True Crime Archive © ${year}`);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Archive Loaded");
+
+});
